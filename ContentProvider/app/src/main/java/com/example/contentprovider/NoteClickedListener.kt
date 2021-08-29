67424bf8b12c3b4e481724bd1008b54ca688db26
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2021-08-29 11:36:50 -0300

    create interfaceNoteClickedListener

---
+++ ContentProvider/app/src/main/java/com/example/contentprovider/NoteClickedListener.kt
@@ -0,0 +1,10 @@
+package com.example.contentprovider
+
+import android.database.Cursor
+
+interface NoteClickedListener {
+
+    fun noteClickedItem(cursor: Cursor)
+    fun noteRemoveItem(cursor: Cursor?)
+
+}
